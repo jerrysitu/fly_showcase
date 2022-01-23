8bbd8b90a51751195b056475a85ce9284d355b1a
Jerry S <jerry@jerrysitu.com>
2022-01-23 11:16:15 -0800

    maybe working

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :showcase, ShowcaseWeb.Endpoint,
-  http: [port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: System.get_env("PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,30 @@
-use Mix.Config
+import Config
 
-app_name =
-  System.get_env("FLY_APP_NAME") ||
-    raise "FLY_APP_NAME not available"
+if config_env() == :prod do
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
+
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  host = System.get_env("HOST") || "#{app_name}.fly.dev"
+
+  config :showcase, ShowcaseWeb.Endpoint,
+    url: [host: host, port: 80],
+    http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: String.to_integer(System.get_env("PORT") || "4000")
+    ],
+    secret_key_base: secret_key_base
+
+  config :showcase, ShowcaseWeb.Endpoint, server: true
+end
